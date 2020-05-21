023133233fb692e78dc60e42df2611d61e8ecf9f
Nick Mwangemi <nickmwangemi@gmail.com>
2020-05-21 18:21:23 +0300

    Initial commit

---
+++ Python Day 3/03_operators.py
@@ -0,0 +1,45 @@
+# Arithmetic Operators 
+## Addition operator +
+greeting = "hello" + " " + "world" # string concatenation
+print(greeting)
+num1 = 2
+num2 = 4
+sum = num1 + num2
+print(sum)
+
+## Subtraction - 
+difference = num2 - num1
+print(difference)
+
+## Multiplication * 
+#multiplying strings to form a string with a repeating sequence
+lots_of_hellos = "\nhello "* 10 # escape character
+print(lots_of_hellos)
+
+product = num1 * num2
+print(product)
+
+## Division /
+result = num2 / num1
+print(result)
+
+## Modulus % 
+modulus = num2 % num1
+print("Modulus",modulus)
+
+
+## Exponent **
+exponent = num1**2
+print(exponent)
+
+
+
+
+
+
+
+# Logical Operators
+# Assignment Operators
+# Bitwise Operators
+# Membership Operators
+
