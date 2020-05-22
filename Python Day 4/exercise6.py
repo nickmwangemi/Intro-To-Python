526d1f524a5507a60b6c06457a3b33532bdcfec6
Nick Mwangemi <nickmwangemi@gmail.com>
2020-05-22 18:32:04 +0300

    Notes on if statements

---
+++ Python Day 4/exercise6.py
@@ -0,0 +1,4 @@
+# Ask the user to input a number
+# Check if number is greater than 100 and less than 300
+# if number is greater than 100 and less than 300, check if the number is even
+# print "Number is even"
