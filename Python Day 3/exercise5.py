8db2bf84ae4ca6858c49218da0810ab864652f34
Nick Mwangemi <nickmwangemi@gmail.com>
2020-05-22 11:38:48 +0300

    Updated exercise with program specs

---
+++ Python Day 3/exercise5.py
@@ -0,0 +1,5 @@
+# Temperature convertor - from Celsius to Farenheit 
+# User to input temperature in Celsius
+# Convert the input to Farenheit then output
+# Output using the 3 methods of outputting data
+# "You entered 30 degrees celsius. The Farenheit equivalent is 45"
